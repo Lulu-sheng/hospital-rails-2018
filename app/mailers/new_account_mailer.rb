e2ec89b4f8c72169fefa4d31e33be00e4fd21144
Lulu Sheng <lulu.sheng@shopify.com>
2018-05-15 19:07:33 -0400

    Show password in email

--- app/mailers/new_account_mailer.rb
+++ app/mailers/new_account_mailer.rb
@@ -2,7 +2,8 @@ class NewAccountMailer < ApplicationMailer
   default from: 'Lulu Sheng lulu.sheng1418@gmail.com'
 
   # make it so that we are able to pass in a user
-  def notice_new_account(new_nurse)
+  def notice_new_account(new_nurse, password)
+    @password = password
     @new_nurse = new_nurse
     @new_employee_record = new_nurse.employee_record
     mail to: @new_employee_record.email, subject: "Welcome To The Hospital, #{@new_employee_record.name}"
